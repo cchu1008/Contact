a25d3852b77073e57bd466f1ae195a577e2f6d48
cchu1008 <cchu.1008@gmail.com>
2017-06-23 00:55:43 -0700

    Added Calendar and Toast for 'saving' appointment date.

--- app/src/main/java/com/example/courtneychu/theapp/AppointmentsActivity.java
+++ app/src/main/java/com/example/courtneychu/theapp/AppointmentsActivity.java
@@ -1,12 +1,24 @@
 package com.example.courtneychu.theapp;
 
+import android.app.DatePickerDialog;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class AppointmentsActivity extends AppCompatActivity {
+    SetDate fromDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +40,19 @@ public class AppointmentsActivity extends AppCompatActivity {
                 goToMainActivity();
             }
         });
+
+        EditText enterDate = (EditText) findViewById(R.id.appDate);
+        fromDate = new SetDate(enterDate, this);
+
+        Button setDateButton = (Button) findViewById(R.id.set_date_button);
+        setDateButton.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v){
+               //TODO: Save appointment date
+               //TODO: See if calendar can be edited
+               goToSetDate(fromDate);
+           }
+        });
     }
 
     private void goToContactsActivity(){
@@ -39,4 +64,15 @@ public class AppointmentsActivity extends AppCompatActivity {
         Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
     }
+
+    private void goToSetDate(SetDate fromDate){
+        Context context = getApplicationContext();
+        CharSequence text = "Your date " + fromDate.toString() + " has been saved";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.setGravity(Gravity.TOP|Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+
 }
---
+++ app/src/main/java/com/example/courtneychu/theapp/SetDate.java
@@ -0,0 +1,82 @@
+package com.example.courtneychu.theapp;
+
+import android.app.DatePickerDialog;
+import android.content.Context;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+/**
+ * Created by Courtney Chu on 6/23/2017.
+ */
+
+public class SetDate implements View.OnFocusChangeListener, DatePickerDialog.OnDateSetListener {
+    private EditText editText;
+    private Calendar myCalendar;
+    private Context ctx;
+
+    public SetDate(EditText editText, Context ctx) {
+        this.editText = editText;
+        this.editText.setOnFocusChangeListener(this);
+        this.ctx = ctx;
+        myCalendar = Calendar.getInstance();
+    }
+
+    @Override
+    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+        String myFormat = "MM/dd/yyyy";
+        SimpleDateFormat sdformat = new SimpleDateFormat(myFormat, Locale.US);
+
+        myCalendar.set(Calendar.YEAR, year);
+        myCalendar.set(Calendar.MONTH, monthOfYear);
+        myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+        editText.setText(sdformat.format(myCalendar.getTime()));
+    }
+
+    @Override
+    public void onFocusChange(View v, boolean hasFocus){
+        if(hasFocus){
+            new DatePickerDialog(this.ctx, this, myCalendar.get(Calendar.YEAR), myCalendar.get(Calendar.MONTH), myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        }
+    }
+
+    @Override
+    public String toString(){
+        return getMonth(myCalendar.get(Calendar.MONTH)) + " " + myCalendar.get(Calendar.DAY_OF_MONTH) + ", " + myCalendar.get(Calendar.YEAR);
+    }
+
+    private String getMonth(int month){
+        switch(month){
+            case 0:
+                return "January";
+            case 1:
+                return "February";
+            case 2:
+                return "March";
+            case 3:
+                return "April";
+            case 4:
+                return "May";
+            case 5:
+                return "June";
+            case 6:
+                return "July";
+            case 7:
+                return "August";
+            case 8:
+                return "September";
+            case 9:
+                return "October";
+            case 10:
+                return "November";
+            case 11:
+                return "December";
+            default:
+                return "January";
+        }
+    }
+}
